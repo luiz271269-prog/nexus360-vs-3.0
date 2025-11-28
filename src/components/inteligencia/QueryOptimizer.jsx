8b6f0c4bc01f972a6ccbd9bf5da77ed5a7b1fa96
Luiz Liesch <luiz271269@gmail.com>
2025-11-28 03:02:38 +0000

    File changes

--- src/components/inteligencia/QueryOptimizer.jsx
+++ src/components/inteligencia/QueryOptimizer.jsx
@@ -56,15 +56,20 @@ export class QueryOptimizer {
 
     let resultado;
     
+    const cacheKey = `${nomeEntidade}_list_${ordenacao}_${limite}`;
+    
     if (usarCache) {
-      resultado = await cacheGlobal.cacheQuery(
-        nomeEntidade,
-        { method: 'list', ordenacao, limite },
-        async () => await entidade.list(ordenacao, limite),
-        ttlCache
-      );
-    } else {
-      resultado = await entidade.list(ordenacao, limite);
+      const cached = localCache.get(cacheKey);
+      if (cached) {
+        this.metricas.cache_hits = (this.metricas.cache_hits || 0) + 1;
+        return cached;
+      }
+    }
+    
+    resultado = await entidade.list(ordenacao, limite);
+    
+    if (usarCache) {
+      localCache.set(cacheKey, resultado, ttlCache);
     }
 
     const tempoQuery = Date.now() - inicioQuery;
