7ffa1887f4bdec040d61787bbeeff2311c4a8b78
Luiz Liesch <luiz271269@gmail.com>
2025-12-16 18:12:09 +0000

    File changes

--- src/components/agenda/PainelPrioridades.jsx
+++ src/components/agenda/PainelPrioridades.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
