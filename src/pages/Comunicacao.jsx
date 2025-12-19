d407f0b7470453f9415ecdf9ad31a6866db9a9c1
Luiz Liesch <luiz271269@gmail.com>
2025-12-19 12:54:23 +0000

    File changes

--- src/pages/Comunicacao.jsx
+++ src/pages/Comunicacao.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import ReactFixHelper from "../components/global/ReactFixHelper";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
