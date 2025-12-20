976d433fb7e9a8245afff02d6bdff1a4159d1ad0
Luiz Liesch <luiz271269@gmail.com>
2025-12-20 13:15:17 +0000

    File changes

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { base44 } from "@/api/base44Client";
 import { useSearchParams } from "react-router-dom";
 
